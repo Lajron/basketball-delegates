5ed744f8d017ae8a95d225779c2f5a14f5f620e6
Lajron <dmilos@elfak.rs>
2023-10-24 14:50:27 +0200

    fix

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static("./public"));
-
-app.listen(3000, () => console.log(`Server started - localhost:3000`));
